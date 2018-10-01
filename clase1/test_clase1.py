04b42f75c4352b81e8c26f02ccdac02d27835600
aleql <harambedcc@gmail.com>
2018-10-01 17:45:42 -0300

    Clase 1, perceptron not

--- clase1/test_clase1.py
+++ clase1/test_clase1.py
@@ -22,7 +22,7 @@ class Test_p_or_test(unittest.TestCase):
         """Call before every test case."""
         self.p_or = perceptron([1, 1], -0.5)
 
-    def test_tablaVerdad(self):
+    def test_tabla_verdad(self):
         assert self.p_or.feed([1, 1]) == 1, "p_or not calculating values correctly"
         assert self.p_or.feed([1, 0]) == 1, "p_or not calculating values correctly"
         assert self.p_or.feed([0, 1]) == 1, "p_or not calculating values correctly"
@@ -34,15 +34,25 @@ class Test_p_nand_test(unittest.TestCase):
         """Call before every test case."""
         self.p_nand = perceptron([-2, -2], 3)
 
-    def test_tablaVerdad(self):
+    def test_tabla_verdad(self):
         assert self.p_nand.feed([1, 1]) == 0, "p_nand not calculating values correctly"
         assert self.p_nand.feed([1, 0]) == 1, "p_nand not calculating values correctly"
         assert self.p_nand.feed([0, 1]) == 1, "p_nand not calculating values correctly"
         assert self.p_nand.feed([0, 0]) == 1, "p_nand not calculating values correctly"
 
+class Test_p_not_test(unittest.TestCase):
+
+    def setUp(self):
+        """Call before every test case."""
+        self.p_not = perceptron(-1, 1)
+
+    def test_tabla_verdad(self):
+        assert self.p_not.feed(1) == 0, "p_not not calculating values correctly"
+        assert self.p_not.feed(0) == 1, "p_not not calculating values correctly"
+
 class Test_p_sum(unittest.TestCase):
 
-    def test_tablaVerdad(self):
+    def test_tabla_verdad(self):
         assert p_sum(1, 1)[0] == 0, "p_sum not calculating values correctly"
         assert p_sum(1, 0)[0] == 1, "p_sum not calculating values correctly"
         assert p_sum(0, 1)[0] == 1, "p_sum not calculating values correctly"
